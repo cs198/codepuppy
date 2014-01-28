42dc9a62f572554ec450e9b8a27095e847ad8ada
Omar Diab <o.s.diab@gmail.com>
2014-01-28 00:37:21 -0800

    db(sections_people): simplify associations between sections and people

--- db/migrate/20140127092414_create_section_students.rb
+++
@@ -1,8 +0,0 @@
-class CreateSectionStudents < ActiveRecord::Migration
-  def change
-    create_table :section_students do |t|
-      t.belongs_to :student, index: true
-      t.belongs_to :section, index: true
-    end
-  end
-end
