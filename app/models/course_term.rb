bd212b1bda9312d15cc97a9039552dfede51d342
Omar Diab <o.s.diab@gmail.com>
2014-01-28 14:52:09 -0800

    models(scopes): remove scopes, use block extensions

--- app/models/course_term.rb
+++ app/models/course_term.rb
@@ -1,5 +1,17 @@
 class CourseTerm < ActiveRecord::Base
-  has_many :people, through: :course_terms_people,
+  has_many :course_terms_people
+  has_many :people, through: :course_terms_people do
+    def students
+      where('course_terms_people.role = ?', 'student')
+    end
+    def admins
+      where('course_terms_people.role = ?', 'admin')
+    end
+    def leaders
+      where('course_terms_people.role = ?', 'leader')
+    end
+  end
+
   has_many :assignments
   has_many :sections
   belongs_to :course
