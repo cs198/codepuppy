cb091dc3fc1eac6cf22c21e473f20c3ba3bfc137
Omar Diab <o.s.diab@gmail.com>
2014-02-01 15:29:07 -0800

    feat(hello_world): hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Namepending::Application.routes.draw do
+  root 'index#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
