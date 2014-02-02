0c5f546e8b12e582914a7e15bbcc93bec2aa5c8a
Omar Diab <o.s.diab@gmail.com>
2014-02-01 16:35:13 -0800

    chore(templates): add templates to rails assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module Namepending
   class Application < Rails::Application
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/templates"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
