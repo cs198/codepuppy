6059273b44eeef21528d75733d86155e2408ff22
Maesen Churchill <maesenc@stanford.edu>
2014-02-08 13:23:25 -0800

    Fixed the bug in navigation -- can now go from home to class to assignment and back correctly.

--- app/assets/javascripts/controllers/class/ClassCtrl.js
+++ app/assets/javascripts/controllers/class/ClassCtrl.js
@@ -1,6 +1,8 @@
 angular.module('codepuppy').controller('ClassCtrl', function($scope, $routeParams) {
     $scope.message = 'Look! I am a class page for ' + $routeParams.classID;
-    $scope.$parent.classes = [$routeParams.classID];
+    $scope.$parent.selectedClass = $routeParams.classID;
+    $scope.$parent.selectedAssignment = null;
+
     var FAKEassignmentsForGivenClass = ['One', 'Two']
     $scope.$parent.assignments = FAKEassignmentsForGivenClass;
 });
