c9e641741464adf53c1c37b8d9e02c186774b032
Omar Diab <o.s.diab@gmail.com>
2014-02-08 18:38:16 -0800

    add syntax highlighted code for submission demo

--- app/assets/javascripts/controllers/course/CourseCtrl.js
+++ app/assets/javascripts/controllers/course/CourseCtrl.js
@@ -3,7 +3,6 @@ angular.module('codepuppy').controller('CourseCtrl',
     $scope.message = 'Look! I am a class page for ' + $routeParams.courseID;
     $scope.$parent.selectedCourse = $routeParams.courseID;
     $scope.$parent.selectedAssignment = null;
-
     var FAKEassignmentsForGivenCourse = ['One', 'Two'];
     $scope.$parent.assignments = FAKEassignmentsForGivenCourse;
 });
--- app/assets/javascripts/controllers/submission/SubmissionCtrl.js
+++ app/assets/javascripts/controllers/submission/SubmissionCtrl.js
@@ -2,9 +2,6 @@ angular.module('codepuppy').controller('SubmissionCtrl', function($scope, $route
     $scope.$parent.selectedCourse     = $routeParams.courseID;
     $scope.$parent.selectedAssignment = $routeParams.assignmentID;
     $scope.$parent.selectedSubmission = $routeParams.submissionID;
-    $scope.message = "I'm a submission page for the course " + $routeParams.courseID;
-    $scope.message+= " and assignment " + $routeParams.assignmentID;
-    $scope.message+= " and submission " + $routeParams.submissionID;
 
     var FAKEsubmissionsForGivenAssignment = ['rawatson_3', 'maesenc_5'];
     $scope.$parent.submissions = FAKEsubmissionsForGivenAssignment;
