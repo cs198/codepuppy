c985cd2f94062884c653ca7051945400beb43ce7
Reid Watson <rawatson@stanford.edu>
2014-02-14 12:33:25 -0800

    Removed all old code for legacy navigation

--- app/assets/app/pages/course/CourseCtrl.js
+++ app/assets/app/pages/course/CourseCtrl.js
@@ -1,8 +1,3 @@
 angular.module('codepuppy').controller('CourseCtrl', function($scope, $routeParams) {
     $scope.message = 'Look! I am a course page for ' + $routeParams.courseID;
-    $scope.$parent.selectedCourse = $routeParams.courseID;
-    $scope.$parent.selectedAssignment = null;
-
-    var FAKEassignmentsForGivenCourse = ['One', 'Two']
-    $scope.$parent.assignments = FAKEassignmentsForGivenCourse;
 });
