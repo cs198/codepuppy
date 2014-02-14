e32b08deeca5c06a0bf8f0027dde5a704e56a129
Reid Watson <rawatson@stanford.edu>
2014-02-14 12:33:25 -0800

    Removed all old code for legacy navigation

--- app/assets/app/pages/home/HomeCtrl.js
+++ app/assets/app/pages/home/HomeCtrl.js
@@ -1,7 +1,3 @@
 angular.module('codepuppy').controller('HomeCtrl', function($scope) {
-    // create a message to display in our view
     $scope.message = 'Welcome screen for CodePuppy.  Select your course...';
-    $scope.$parent.assignments = null;
-    $scope.$parent.selectedCourse = null;
-    $scope.$parent.selectedAssignment = null;
 });
