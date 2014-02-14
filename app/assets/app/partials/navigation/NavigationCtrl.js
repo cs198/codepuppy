3e63a24bc459ed2f50eac79dd2451764a8ab33a9
Omar Diab <o.s.diab@gmail.com>
2014-02-13 22:54:41 -0800

    feat(navigation): split nav into directives, controllers, templates

--- app/assets/app/partials/navigation/NavigationCtrl.js
+++ app/assets/app/partials/navigation/NavigationCtrl.js
@@ -1,3 +1,12 @@
 angular.module('codepuppy').controller('NavigationCtrl', function($scope) {
-  $scope.courses = ['cs106a', 'cs106b', 'cs106x', 'cs142'];
+  //TODO: make these api calls
+  var courses = ['cs106a', 'cs106b', 'cs142'];
+  var assignments = ['Assignment1', 'Assignment2'];
+  var submissions = ['rawatson_1', 'rawatson_2', 'maesenc_1'];
+
+  $scope.navigationSelectors = [
+    { title: 'Courses', elements: courses },
+    { title: 'Assignments', elements: assignments},
+    { title: 'Submissions', elements: submissions }
+  ];
 });
