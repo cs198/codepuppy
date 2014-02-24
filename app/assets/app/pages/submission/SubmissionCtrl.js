7be57e8481357a140132ddfc5096fd4125dd757b
Maesen Churchill <maesenc@stanford.edu>
2014-02-24 11:12:27 -0800

    Added a basic modal with text databinding to the Submission page.

--- app/assets/app/pages/submission/SubmissionCtrl.js
+++ app/assets/app/pages/submission/SubmissionCtrl.js
@@ -1,5 +1,21 @@
-angular.module('codepuppy').controller('SubmissionCtrl', function($scope, $routeParams) {
+angular.module('codepuppy').controller('SubmissionCtrl', function($scope, $routeParams, $modal) {
     $scope.message = "I'm a submission page for the course " + $routeParams.courseID;
     $scope.message+= " and assignment " + $routeParams.assignmentID;
     $scope.message+= " and submission " + $routeParams.submissionID;
+
+    
+
+    $scope.submissionClicked = function() {
+
+		var commentModal = $modal.open({
+		templateUrl: '/assets/partials/commentModal/commentModal.html',
+		controller: CommentModalCtrl
+		});
+
+		commentModal.result.then(function (commentBody) {
+			// TODO: Create a comment directive, set its text to commentBody.
+			$scope.commentBody = commentBody;
+		});
+	};
+
 });
---
+++ app/assets/app/partials/commentModal/commentModal.js
@@ -0,0 +1,16 @@
+var CommentModalCtrl = function($scope, $modalInstance) {
+
+    $scope.commentBody = {};
+
+    //TODO: Set initial value to value of comment clicked on.
+    $scope.commentBody.comment = "Initial value";
+    
+    $scope.ok = function() {
+       $modalInstance.close($scope.commentBody.comment);
+    };
+
+    $scope.cancel = function() {
+       $modalInstance.dismiss('cancel');
+    };
+
+};
