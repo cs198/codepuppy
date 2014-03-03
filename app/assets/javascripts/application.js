97c11000ffd95f50c825fd060596bdd4ec221c38
Omar Diab <o.s.diab@gmail.com>
2014-03-02 20:31:49 -0800

    use local js and css libs for fewer http, simplicity

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,37 +1,6 @@
-//= require_self
+//= require_tree ./vendor/angular/core
+//= require_tree ./vendor/angular/modules
+//= require_tree ./vendor/jquery
+//= require_tree ./vendor/bootstrap
+//= require_tree ./vendor
 //= require_tree .
-
-// TODO: this needs to be called after code is displayed
-SyntaxHighlighter.all();
-
-codepuppy = angular.module('codepuppy', ['ngRoute']);
-
-codepuppy.config(function($routeProvider) {
-  //if ($user == 'student') {
-  if (true) {
-    $routeProvider
-    .when('/', {
-      templateUrl : '/assets/home.html',
-      controller  : 'MainCtrl'
-    })
-    .when('/:classID', {
-      templateUrl : '/assets/class.html',
-      controller  : 'ClassCtrl'
-    })
-    .when('/:classID/:assignmentID', {
-      templateUrl : '/assets/assignment.html',
-      controller  : 'AssignmentCtrl'
-    })
-    .when('/:classID/:assignmentID/:submissionID', {
-      templateUrl : '/assets/submission.html',
-      controller  : 'SubmissionCtrl'
-    })
-
-//} else if ($user == 'section leader') {
-//     more stuff
-//} else if ($user == 'lecturer') {
-//     more stuff
-
-  }
-});
-
