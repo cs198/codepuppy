cf1a859f9332f4e91d572cab3d9de665f0b5a460
Anthony J Mainero <anthony@dn51us9t.sunet>
2014-03-08 20:55:41 -0800

    Added creation feature for submission files, fixed a few typos/bugs

--- app/models/submission_file.rb
+++ app/models/submission_file.rb
@@ -2,6 +2,6 @@ class SubmissionFile < ActiveRecord::Base
   belongs_to :submission
 
   validates_presence_of :submission_id
-  validates_presence_of :data
+  validates_presence_of :path
   validates_presence_of :data
 end
