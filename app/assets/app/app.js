83b2011b7d179cd1652f9a8d8878adbc03916af5
Maesen Churchill <maesenc@stanford.edu>
2014-03-10 21:47:28 -0700

    Set up directories and modified routes by role.  Files in each directory are clones.

--- app/assets/app/app.js
+++ app/assets/app/app.js
@@ -5,21 +5,35 @@ codepuppy = angular.module('codepuppy', ['ngRoute', 'ui.bootstrap',
   'angularFileUpload']);
 
 codepuppy.config(function($routeProvider) {
+  // TODO: use session information to determine role.  Should be one of 'student', 'leader'
+  var role = 'leader';
+
+  // Student-specific routes:
+  if(role === 'student') {
+    $routeProvider.when('/:courseID/:assignmentID/submit', {
+      templateUrl: '/assets/pages/assignment/' + role + '/assignment.html',
+      controller: 'AssignmentCtrl'
+    });
+  }
+
   $routeProvider
   .when('/', {
-    templateUrl: '/assets/pages/home/home.html',
+    templateUrl: '/assets/pages/home/' + role + '/home.html',
     controller: 'HomeCtrl'
   })
   .when('/:courseID', {
-    templateUrl: '/assets/pages/course/course.html',
+    templateUrl: '/assets/pages/course/' + role + '/course.html',
     controller: 'CourseCtrl'
   })
   .when('/:courseID/:assignmentID', {
-    templateUrl: '/assets/pages/assignment/assignment.html',
+    templateUrl: '/assets/pages/assignment/' + role + '/assignment.html',
     controller: 'AssignmentCtrl'
   })
   .when('/:courseID/:assignmentID/:submissionID', {
-    templateUrl: '/assets/pages/submission/submission.html',
+    templateUrl: '/assets/pages/submission/' + role + '/submission.html',
     controller: 'SubmissionCtrl'
   });
+
+  
+
 });
