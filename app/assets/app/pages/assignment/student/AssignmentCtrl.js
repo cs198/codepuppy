90a6b1191c3a367bf46e057b96bcef0256115266
Maesen Churchill <maesenc@stanford.edu>
2014-03-11 14:12:26 -0700

    Added link to submit page from assignment page.

--- app/assets/app/pages/assignment/student/AssignmentCtrl.js
+++ app/assets/app/pages/assignment/student/AssignmentCtrl.js
@@ -1,4 +1,6 @@
 angular.module('codepuppy').controller('AssignmentCtrl', function($scope, $routeParams, $fileUploader) {
-    $scope.message = "I'm an assignment page for the course " + $routeParams.classID;
+    $scope.message = "I'm an assignment page for the course " + $routeParams.courseID;
     $scope.message+= " and assignment " + $routeParams.assignmentID;
+
+    $scope.uploadPath = '/#/' + $routeParams.courseID + '/' + $routeParams.assignmentID + '/submit';
 });
