7b7f66941eb6c5fb5d5ec8b7a7ffdfcf97a2db8f
Anthony J Mainero <anthony@dn0a231051.sunet>
2014-03-15 13:45:11 -0700

    Adds getting assignment name and description on assignment pages

--- app/assets/app/pages/assignment/student/StudentAssignmentCtrl.js
+++ app/assets/app/pages/assignment/student/StudentAssignmentCtrl.js
@@ -1,6 +1,23 @@
-angular.module('codepuppy').controller('StudentAssignmentCtrl', function($scope, $routeParams, $fileUploader) {
-    $scope.message = "I'm an assignment page for the course " + $routeParams.courseID;
-    $scope.message+= " and assignment " + $routeParams.assignmentID;
+angular.module('codepuppy').controller('StudentAssignmentCtrl', ['$scope', '$routeParams', '$fileUploader', '$http', function($scope, $routeParams, $fileUploader, $http) {
 
-    $scope.uploadPath = '/#/' + $routeParams.courseID + '/' + $routeParams.assignmentID + '/submit';
+  var getAssignment = function() {
+    $http({method: 'GET', url: '/assignments/' + $routeParams.assignmentID + '.json'})
+    .success(function(data, status, headers, config) {
+      console.log(data);
+      $scope.assignmentDescription = data.description;
+      $scope.assignmentName = data.name;
+      if (!$scope.assignmentDescription) {
+        $scope.assignmentDescription = "";
+      }
+      if (!$scope.assignmentName) {
+        $scope.assignmentName = "";
+      }
+    });
+  };
+  getAssignment();
+
+  $scope.message = "I'm an assignment page for the course " + $routeParams.courseID;
+  $scope.message+= " and assignment " + $routeParams.assignmentID;
+
+  $scope.uploadPath = '/#/' + $routeParams.courseID + '/' + $routeParams.assignmentID + '/submit';
 });
