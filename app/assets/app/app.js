dfb957016f7e6b5ba648e631008224fdc9bb1a87
Omar Diab <o.s.diab@gmail.com>
2014-03-16 16:13:10 -0700

    remove route for session verify, fix route for submitting code, decomp capitalization (bandaid)

--- app/assets/app/app.js
+++ app/assets/app/app.js
@@ -5,13 +5,21 @@ codepuppy = angular.module('codepuppy', ['ngRoute', 'ngCookies', 'ui.bootstrap',
   'angularFileUpload']);
 
 codepuppy.config(function($routeProvider) {
-  // TODO: use session information to determine role.  Should be one of 'student', 'leader'
+  // TODO: use session information to determine role.  Should be one of
+  // 'student', 'leader'
+  var upperCaseFirst = function(str) {
+    if (str.length === 0) {
+        return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
   var role = 'student';
-  var capRole = 'Student';
+  var capRole = upperCaseFirst(role);
 
   // Student-specific routes:
-  if(role === 'student') {
-    $routeProvider.when('/:courseID/:assignmentID/submit', {
+  if (role === 'student') {
+    $routeProvider.when('/courses/:courseID/assignments/:assignmentID/submit', {
       templateUrl: '/assets/pages/fileUpload/' + role + '/upload.html',
       controller: 'FileUploadCtrl'
     });
@@ -23,22 +31,21 @@ codepuppy.config(function($routeProvider) {
     controller: capRole + 'HomeCtrl'
   })
   .when('/courses/:courseID', {
-    templateUrl: '/assets/pages/course/' + role +'/' + role + 'Course.html',
+    templateUrl: '/assets/pages/course/' + role + '/' + role +
+        'Course.html',
     controller: capRole + 'CourseCtrl'
   })
   .when('/courses/:courseID/assignments/:assignmentID', {
-    templateUrl: '/assets/pages/assignment/' + role + '/' + role + 'Assignment.html',
+    templateUrl: '/assets/pages/assignment/' + role + '/' + role +
+        'Assignment.html',
     controller: capRole + 'AssignmentCtrl'
   })
   .when('/courses/:courseID/assignments/:assignmentID/' +
         'submissions/:submissionID',
   {
-    templateUrl: '/assets/pages/submission/' + role + '/' + role + 'Submission.html',
+    templateUrl: '/assets/pages/submission/' + role + '/' + role +
+        'Submission.html',
     controller: capRole + 'SubmissionCtrl'
-  })
-  .when('/sessions/verify', {
-    templateUrl: '/assets/pages/sessions/verify/sessions-verify.html',
-    controller: 'SessionsVerifyCtrl'
   })
   .otherwise({
     templateUrl: '/assets/errorPages/404/404.html',
