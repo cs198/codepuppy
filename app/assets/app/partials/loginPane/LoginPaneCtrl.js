770de3812d6da0aa916304c2885af2e9472026ca
Omar Diab <o.s.diab@gmail.com>
2014-03-15 21:37:13 -0700

    add session service and hook it into app

--- app/assets/app/partials/loginPane/LoginPaneCtrl.js
+++ app/assets/app/partials/loginPane/LoginPaneCtrl.js
@@ -1,41 +1,9 @@
-angular.module('codepuppy').controller('LoginPaneCtrl',
-    ['$scope', '$location', '$http', function($scope, $location, $http)
-    {
-        // TODO: rather than use $scope var, use service that manages session
-        $scope.loggedIn = false;
-
-        $scope.loginWebauth = function() {
-            var WEBAUTH_LOCATION =
-                'https://www.stanford.edu/~rawatson/cgi-bin/codepuppy/auth.py';
-
-            var redirectToWebauth = function(token)
-            {
-                var baseUrl = $location.protocol() + '://' + $location.host() +
-                    ':' + $location.port();
-                var params = {
-                    forward_url: $location.path(),
-                    verify_url: baseUrl + '/#/sessions/verify',
-                    token: token
-                };
-
-                var request_url = WEBAUTH_LOCATION + '?' + $.param(params);
-                window.location = request_url;
-            };
-
-            $http({
-                method: 'POST',
-                url: '/sessions/generate'
-            }).success(function(data) {
-                redirectToWebauth(data.token);
-            }).error(function(data, status, headers, config) {
-                // TODO: handle gracefully
-                alert('Could not generate token!');
-                console.log(data);
-            });
-        };
-
-        $scope.logout = function() {
-            alert('logout not implemented');
-        };
-    }
-    ]);
+angular.module('codepuppy').controller('LoginPaneCtrl', ['$scope', 'session',
+        function($scope, session)
+{
+    $scope.logout = function() {
+        session.logout();
+        alert('You just logged out.');
+        window.location = 'http://cs198.stanford.edu';
+    };
+}]);
