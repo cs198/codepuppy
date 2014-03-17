25da6b072873f324d97c7cbe61e737f58b4af1e9
Omar Diab <o.s.diab@gmail.com>
2014-03-17 16:46:25 -0700

    configure application scss load path; remove scss from app folder; add basic text styles

--- config/application.rb
+++ config/application.rb
@@ -23,4 +23,8 @@ module Namepending
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+
+  Sass::Engine::DEFAULT_OPTIONS[:load_paths].tap do |load_paths|
+    load_paths << "#{Rails.root}/app/assets/stylesheets"
+  end
 end
