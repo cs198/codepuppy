e18dc0dc5cd380ee26a67c5464d863814469d878
Maesen Churchill <maesenc@stanford.edu>
2014-03-17 15:00:06 -0700

    Created a form for announcements.

--- app/assets/app/pages/newAnnouncement/leader/NewAnnouncementCtrl.js
+++ app/assets/app/pages/newAnnouncement/leader/NewAnnouncementCtrl.js
@@ -1,3 +1,16 @@
-angular.module('codepuppy').controller('NewAnnouncementCtrl', ['$scope', '$routeParams', '$fileUploader', function($scope, $routeParams, $fileUploader) {
-    
+angular.module('codepuppy').controller('NewAnnouncementCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+	$scope.formError = false;
+
+    $scope.submit = function() {
+		if($scope.announcement.$valid) {
+			$scope.formError = false;
+			// POST
+		} else {
+			$scope.formError = true;
+		}
+    };
+
+    $scope.cancel = function() {
+		// Redirect
+    };
 }]);
