7c5651444b782f2e8f68c14f0c89df99b228bf7c
Omar Diab <o.s.diab@gmail.com>
2014-03-18 21:36:03 -0700

    add title of course to header

--- app/assets/app/partials/header/HeaderCtrl.js
+++ app/assets/app/partials/header/HeaderCtrl.js
@@ -1,2 +1,15 @@
-angular.module('codepuppy').controller('HeaderCtrl', function($scope) {
-});
+angular.module('codepuppy').controller('HeaderCtrl', [
+'$scope', '$http', '$stateParams',
+function($scope, $http, $stateParams)
+{
+    if ($stateParams.courseID) {
+        $http({
+            url: '/courses/' + $stateParams.courseID + '.json',
+            method: 'GET'
+        }).success(function(data, status, headers, config) {
+            if (data.course_name) {
+                $scope.title = data.course_code + ': ' + data.course_name;
+            }
+        });
+    }
+}]);
