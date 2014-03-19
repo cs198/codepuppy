46d8c3eefd3670e805b8c1a95c23882ae7f220c2
Omar Diab <o.s.diab@gmail.com>
2014-03-18 19:30:31 -0700

    use multiple views to have separate views for header, sidebar, content

--- app/assets/app/app.js
+++ app/assets/app/app.js
@@ -20,51 +20,94 @@ codepuppy.config(function($stateProvider, $urlRouterProvider) {
     // Student-specific routes:
     if (role === 'student') {
         $stateProvider.state('submit', {
-            url: '/courses/:courseID/assignments/:assignmentID/submit',
-            templateUrl: '/assets/pages/fileUpload/' + role +
-                '/upload.html',
-            controller: 'FileUploadCtrl'
+            url: '^/courses/:courseID/assignments/:assignmentID/submit',
+            views: {
+                'content': {
+                    templateUrl: '/assets/pages/fileUpload/' + role +
+                        '/upload.html',
+                    controller: 'FileUploadCtrl'
+                },
+                'sidebar': { template: '<sidebar />' },
+                'header': { template: '<header />' }
+            }
         });
     }
 
     // Leader-specific routes:
     if (role === 'leader') {
         $stateProvider.state('newAnnouncement', {
-            url: '/courses/:courseID/newAnnouncement',
-            templateUrl: '/assets/pages/newAnnouncement/' + role +
-                '/announcement.html',
-            controller: 'NewAnnouncementCtrl'
+            url: '^/courses/:courseID/newAnnouncement',
+            views: {
+                'content': {
+                    templateUrl: '/assets/pages/newAnnouncement/' + role +
+                        '/announcement.html',
+                    controller: 'NewAnnouncementCtrl'
+                },
+                'sidebar': { template: '<sidebar />' },
+                'header': { template: '<header />' }
+            }
         });
     }
 
     $stateProvider.state('home', {
-        url: '/',
-        templateUrl: '/assets/pages/home/' + role + '/' + role + 'Home.html',
-        controller: capRole + 'HomeCtrl'
+        url: '^',
+        views: {
+            'content': {
+                templateUrl: '/assets/pages/home/' + role + '/' + role +
+                    'Home.html',
+                controller: capRole + 'HomeCtrl'
+            },
+            'sidebar': { template: '<sidebar />' },
+            'header': { template: '<header />' }
+        }
     })
     .state('course', {
-        url: '/courses/:courseID',
-        templateUrl: '/assets/pages/course/' + role + '/' + role +
-            'Course.html',
-        controller: capRole + 'CourseCtrl'
+        url: '^/courses/:courseID',
+        views: {
+            'content': {
+                templateUrl: '/assets/pages/course/' + role + '/' + role +
+                    'Course.html',
+                controller: capRole + 'CourseCtrl'
+            },
+            'sidebar': { template: '<sidebar />' },
+            'header': { template: '<header />' }
+        }
     })
     .state('assignment', {
-        url: '/courses/:courseID/assignments/:assignmentID',
-        templateUrl: '/assets/pages/assignment/' + role + '/' + role +
-            'Assignment.html',
-        controller: capRole + 'AssignmentCtrl'
+        url: '^/courses/:courseID/assignments/:assignmentID',
+        views: {
+            'content': {
+                templateUrl: '/assets/pages/assignment/' + role + '/' + role +
+                    'Assignment.html',
+                controller: capRole + 'AssignmentCtrl'
+            },
+            'sidebar': { template: '<sidebar />' },
+            'header': { template: '<header />' }
+        }
     })
     .state('submission', {
-        url: '/courses/:courseID/assignments/:assignmentID/' +
+        url: '^/courses/:courseID/assignments/:assignmentID/' +
              'submissions/:submissionID',
-        templateUrl: '/assets/pages/submission/' + role + '/' + role +
-            'Submission.html',
-        controller: capRole + 'SubmissionCtrl'
+        views: {
+            'content': {
+                templateUrl: '/assets/pages/submission/' + role + '/' + role +
+                    'Submission.html',
+                controller: capRole + 'SubmissionCtrl'
+            },
+            'sidebar': { template: '<sidebar />' },
+            'header': { template: '<header />' }
+        }
     })
     .state('404', {
-        url: '/404',
-        templateUrl: '/assets/errorPages/404/404.html',
-        controller: '404Ctrl'
+        url: '^/404',
+        views: {
+            'content': {
+                templateUrl: '/assets/errorPages/404/404.html',
+                controller: '404Ctrl'
+            },
+            'sidebar': { template: '<sidebar />' },
+            'header': { template: '<header />' }
+        }
     });
 
     $urlRouterProvider.otherwise('/404');
