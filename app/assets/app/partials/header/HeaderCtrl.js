92de8cf0dac733d167dfd66e878818496558cebf
Omar Diab <o.s.diab@gmail.com>
2014-03-18 21:42:43 -0700

    add caching to header title

--- app/assets/app/partials/header/HeaderCtrl.js
+++ app/assets/app/partials/header/HeaderCtrl.js
@@ -5,7 +5,8 @@ function($scope, $http, $stateParams)
     if ($stateParams.courseID) {
         $http({
             url: '/courses/' + $stateParams.courseID + '.json',
-            method: 'GET'
+            method: 'GET',
+            cache: true
         }).success(function(data, status, headers, config) {
             if (data.course_name) {
                 $scope.title = data.course_code + ': ' + data.course_name;
