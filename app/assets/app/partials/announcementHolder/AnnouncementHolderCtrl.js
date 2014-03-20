4a6c8b7c57ffa2a22e360315af34572f2cc60dd4
Maesen Churchill <maesenc@stanford.edu>
2014-03-19 20:35:37 -0700

    Fixed the formatting of the date for annoucements.

--- app/assets/app/partials/announcementHolder/AnnouncementHolderCtrl.js
+++ app/assets/app/partials/announcementHolder/AnnouncementHolderCtrl.js
@@ -1,2 +1,4 @@
 angular.module('codepuppy').controller('AnnouncementHolderCtrl', ['$scope', function($scope) {
+	var createTime = moment($scope.announcement.created_at);
+	$scope.createdAtFormatted = createTime.format('MMM DD, YYYY, hh:mm A');
 }]);
